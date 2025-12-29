9cf9ff19fb048ef7162257bdabda7a893d892dbb
Aleph Lau <aleph.lau@indabasolutions.com>
2025-12-28 18:57:58 -0800

    feat: install and enable Pimcore Studio UI bundle

--- config/bundles.php
+++ config/bundles.php
@@ -1,10 +1,12 @@
 <?php
 use Pimcore\Bundle\GenericDataIndexBundle\PimcoreGenericDataIndexBundle;
 use Pimcore\Bundle\StudioBackendBundle\PimcoreStudioBackendBundle;
+use Pimcore\Bundle\StudioUiBundle\PimcoreStudioUiBundle;
 
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Pimcore\Bundle\GenericExecutionEngineBundle\PimcoreGenericExecutionEngineBundle::class => ['all' => true],
     PimcoreGenericDataIndexBundle::class => ['all' => true],
     PimcoreStudioBackendBundle::class => ['all' => true],
+    PimcoreStudioUiBundle::class => ['all' => true],
     ];
