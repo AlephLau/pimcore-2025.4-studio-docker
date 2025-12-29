97107156ad6bde506aa4a987a9e40ad3fbce2306
Aleph Lau <aleph.lau@indabasolutions.com>
2025-12-28 17:56:23 -0800

    feat: add Generic Data Index bundle with OpenSearch

--- config/bundles.php
+++ config/bundles.php
@@ -1,6 +1,8 @@
 <?php
+use Pimcore\Bundle\GenericDataIndexBundle\PimcoreGenericDataIndexBundle;
 
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Pimcore\Bundle\GenericExecutionEngineBundle\PimcoreGenericExecutionEngineBundle::class => ['all' => true],
-];
+    PimcoreGenericDataIndexBundle::class => ['all' => true],
+    ];
