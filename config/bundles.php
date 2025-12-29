70b2c72e05238f268ecf7454582fd07a88549cd4
Aleph Lau <aleph.lau@indabasolutions.com>
2025-12-28 18:44:27 -0800

    feat: enable Pimcore Studio Backend bundle and security firewall

--- config/bundles.php
+++ config/bundles.php
@@ -1,8 +1,10 @@
 <?php
 use Pimcore\Bundle\GenericDataIndexBundle\PimcoreGenericDataIndexBundle;
+use Pimcore\Bundle\StudioBackendBundle\PimcoreStudioBackendBundle;
 
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Pimcore\Bundle\GenericExecutionEngineBundle\PimcoreGenericExecutionEngineBundle::class => ['all' => true],
     PimcoreGenericDataIndexBundle::class => ['all' => true],
+    PimcoreStudioBackendBundle::class => ['all' => true],
     ];
